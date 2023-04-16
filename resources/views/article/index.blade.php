59a153c31c3385295fa1ce1b42dfd816a6428143
tonweb-ja <tonweb.saitama@gmail.com>
2023-04-16 13:01:32 +0900

    breezeインストール、ルーティング設定

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,17 @@
+@extends('common/common')
+@section('content')
+<main>
+    <div class="flex">
+        <section class="text-gray-400">
+            <p>カテゴリで絞る</p>
+        </section>
+        <section>
+            <article>
+                <a href="#">
+                    コンテンツエリア
+                </a>
+            </article>
+        </section>
+    </div>
+</main>
+@endsection
