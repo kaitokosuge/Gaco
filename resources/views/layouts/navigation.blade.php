db76717705b2baf16eaaa85764d5fb26423245a5
tonweb-ja <tonweb.saitama@gmail.com>
2023-04-27 02:28:36 +0900

    /削除

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex items-center">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a class="text-5xl"href="{{ route('/') }}">
+                    <a class="text-5xl" href="/">
                         🐸{{--<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />--}}
                     </a>
                 </div>
